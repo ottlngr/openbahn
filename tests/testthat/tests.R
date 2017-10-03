fd25c9b27c9017945827d725fbc07c865513226b
ottlngr <dev@ottolinger.de>
2017-10-03 20:42:43 +0200

    Some unit tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -1,4 +1,30 @@
+l <- locationNameApi("Mainz Hbf")
+a <- arrivalBoardApi("008000240", Sys.Date() + 1, "12:00:00")
+d <- arrivalBoardApi("008000240", Sys.Date() + 1, "12:00:00")
+j <- journeyDetailApi(a$content$ArrivalBoard$Arrival$JourneyDetailRef$ref[1])
+
 test_that("Results have the right class", {
-  l <- locationNameApi("Mainz Hbf")
+  
   expect_true(class(l) == "openbahn_locationName")
+  expect_true(class(a) == "openbahn_arrivalBoard")
+  expect_true(class(d) == "openbahn_departureBoard")
+  expect_true(class(j) == "openbahn_journeyDetail")
+  
+})
+
+test_that("Results have named objects", {
+  
+  expect_named(l, c("content", "path", "response"), ignore.order = TRUE)
+  expect_named(a, c("content", "path", "response"), ignore.order = TRUE)
+  expect_named(d, c("content", "path", "response"), ignore.order = TRUE)
+  expect_named(j, c("content", "path", "response"), ignore.order = TRUE)
+  
+})
+
+test_that("arrivalBoardApi() throws errors", {
+  
+  expect_error(arrivalBoardApi(date = Sys.Date() + 1, time = "12:00:00"), "No id provided.")
+  expect_error(arrivalBoardApi(id = "008000240", time = "12:00:00"), "No date provided.")
+  expect_error(arrivalBoardApi(id = "008000240", date = Sys.Date() + 1), "No time provided.")
+  
 })
